237eaf4ab6213b97902cd7aa6e98f7f6513edd37
bogdanmfometescu <mbogdan.fometescu@gmail.com>
2023-03-29 20:25:27 +0300

    added second exercise for map function

--- PRACTICE/GENERAL_KNOWLEDGE/BUILT_IN_FUNC/MAP_FUNC/practice/map+func_practice.py
+++ PRACTICE/GENERAL_KNOWLEDGE/BUILT_IN_FUNC/MAP_FUNC/practice/map+func_practice.py
@@ -1,8 +1,8 @@
 # Exercise 1  - Sum some random numbers from user input
 
 print("**********************")
-print("SUM OF 5 NUMBERS FROM USER INPUT")
-numbers = input("Please enter 5 numbers separated by a comma :").split(",")
+print("SUM OF  NUMBERS FROM USER INPUT")
+numbers = input("Please enter few  numbers separated by a comma :").split(",")
 
 # Make a list from the numbers the user entered
 numbers_list = [numbers]
@@ -11,6 +11,34 @@ numbers_list = [numbers]
 flat_list = [item for sublist in numbers_list for item in sublist]
 
 # Get the sum of the elements in the list using map() and sum() function
-result = sum(list(map(int, flat_list)))
+number_as_integer = list(map(int, flat_list))
 
-print(f"The sum of the numbers is : {result}")
+sum_of_numbers = sum(list(map(int, flat_list)))
+
+print(f"The sum  is : {sum_of_numbers}")
+
+# Exercise 2 - Find Odd and Prime numbers from user input
+# We will use the same numbers from exercise 1, so we don't duplicate code
+
+
+results = list(map(lambda x: x % 2 == 0, number_as_integer))
+
+results_lst = list(zip(number_as_integer, results))
+
+for result, number in enumerate(results_lst):
+    print(f"{results_lst[result][0]} is  {results_lst[result][1]}")
+
+print("*************************************************")
+
+
+# This can be coded similar without map() by using a simple function
+
+def find_odd_and_prime_num(num_list):
+    for item in num_list:
+        if item % 2 == 0:
+            print(f"{item} is Prime")
+        else:
+            print(f"{item} is Odd")
+
+
+find_odd_and_prime_num(number_as_integer)
