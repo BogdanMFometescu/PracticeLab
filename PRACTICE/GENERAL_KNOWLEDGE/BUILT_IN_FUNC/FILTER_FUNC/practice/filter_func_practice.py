21bb66e55662ec4eff9d60db0d6566fcd1e06e2c
bogdanmfometescu <mbogdan.fometescu@gmail.com>
2023-04-02 12:46:40 +0300

    added filter practice

--- PRACTICE/GENERAL_KNOWLEDGE/BUILT_IN_FUNC/FILTER_FUNC/practice/filter_func_practice.py
+++ PRACTICE/GENERAL_KNOWLEDGE/BUILT_IN_FUNC/FILTER_FUNC/practice/filter_func_practice.py
@@ -1,3 +1,28 @@
+grades = {"John": 8.6,
+          "Alice": 7.9,
+          "Bob": 6.7,
+          "Derek": 9.9}
 
 
+def filtering_function_by_condition(pair):
+    key, values = pair
+    if values < 8:
+        return True
+    else:
+        return False
 
+
+def filtering_function_by_key(pair):
+    key, value = pair
+    wanted_key = ["Alice", "Bob"]
+    if key in wanted_key:
+        return True
+    else:
+        return False
+
+
+results = list(filter(filtering_function_by_condition, grades.items()))
+print(results)
+
+results2 = list(filter(filtering_function_by_key, grades.items()))
+print(results2)
